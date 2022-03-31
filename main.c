01ea90dbb7ae268822931e61c474a3efde860d7d
Mateusz3124 <95550799+Mateusz3124@users.noreply.github.com>
2022-03-31 14:16:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,53 +11,66 @@
 #define cpath 2
 #define checkconnection 1
 
-int mode = -1; //tryb uruchomienia programu -1 nie wybrano, 0 generate, 1 check_connection, 2 path
+int mode = -1; 
 double min_weight = 0.0;
 double max_weight = 1.0;
 int rows = 1;
 int columns = 1;
-int m = 1; // 1 all, 2 connected, 3 random 
+int m = 1; 
 char *output = "text";
 char *input = "text";
-int check_connection = 0; // czy odpalac BFSa
-int edges = 1; // metoda tworzenia krawedzi
-int npaths = 0; // liczba siciezek ktore trza znalezc
-int *paths; // tablic przehowujaca poczatki i konce sciezek ktore trzeba znalezc w formacie pocz1 kon1 pocz2 ...
+int check_connection = 0; 
+int edges = 1; 
+int npaths = 0; 
+int *paths; 
 
-int get_options (int argc ,char** argv){
-    int i = 1; // zmienna do iterowania po argumentach
-    if(argc<3){ // kazdy mode przyjmuje przynajmniej 1 argument
+int get_options (int argc ,char** argv)
+{
+    int i = 1;
+    if(argc<3)
+    { 
         return 1;
     }
-    if (!strcmp(argv[i], "--generate")){
+    if (!strcmp(argv[i], "--generate"))
+    {
         mode = 0;
-        if(argc % 2 == 0){ // kazda opcja wymaga podania 1 wartosci
+        if(argc % 2 == 0)
+	{ 
             return 1;
         }
         output = argv[++i];
         while(i<argc-1){
             i++;
-            if (!strcmp(argv[i], "--min-weight")){
+            if (!strcmp(argv[i], "--min-weight"))
+	    {
                 if (atof(argv[++i]) != 0 || argv[i+1] == "0") 
                     min_weight = atof(argv[i]);
                 else
                    return 1;
-            }else if (!strcmp(argv[i], "--max-weight")){
+            }
+		else if (!strcmp(argv[i], "--max-weight"))
+	    {
                 if (atof(argv[++i]) != 0 || argv[i+1] == "0") 
                     max_weight = atof(argv[i]);
                 else
                     return 1;
-            }else if (!strcmp(argv[i], "--rows")){
+            }
+		else if (!strcmp(argv[i], "--rows"))
+		{
                 if (atoi(argv[++i]) != 0 || argv[i+1] == "0") 
                     rows = atoi(argv[i]);
                 else
                    return 1;
-            }else if (!strcmp(argv[i], "--columns")){
+            }
+		else if (!strcmp(argv[i], "--columns"))
+		{
                 if (atoi(argv[++i]) != 0 || argv[i+1] == "0") 
                     columns = atoi(argv[i]);
                 else
                     return 1;
-            }else if (!strcmp(argv[i], "--edges")){
+            }
+		else if (!strcmp(argv[i], "--edges"))
+		{
                 i++;
                 if (!strcmp(argv[i], "all"))
                     edges = 0;
@@ -67,21 +80,29 @@ int get_options (int argc ,char** argv){
                     edges = 2;
                 else 
                     return 1;
-            }else{
+            }
+		else
+		{
                 return 1;
             }
         }
         return 0;
-    }else if(!strcmp(argv[i], "--check-connection")){
+    }
+	else if(!strcmp(argv[i], "--check-connection"))
+    {
         mode = 1;
-        if(argc != 3){ // przyjmuje tylko nazwe pliku
+        if(argc != 3)
+	{ 
             return 1;
         }
         input = argv[++i];
         return 0;
-    }else if(!strcmp(argv[i], "--path")){
+    }
+	else if(!strcmp(argv[i], "--path"))
+	{
         mode = 2;
-        if(argc<4){
+        if(argc<4)
+	{
             return 1;
         }
         input = argv[++i];
@@ -90,43 +111,53 @@ int get_options (int argc ,char** argv){
             npaths = atoi(argv[i]);
         else
             return 1;
-        if(argc != 2*npaths+4){ // pocz. i kon. kazdej sciezki byc musi
+        if(argc != 2*npaths+4)
+	{
             return 1;
         }
         paths = malloc(2*npaths*sizeof(int));
-        for (int j=0; j<2*npaths; j++){
+        for (int j=0; j<2*npaths; j++)
+	{
             i++;
-            if (atoi(argv[i]) != 0 || argv[i] == "0"){
+            if (atoi(argv[i]) != 0 || argv[i] == "0")
+	    {
                 paths[j] = atoi(argv[i]);
             }else{
                 return 1;
             }
         }
         return 0;
-    }else{
+    }else
+	{
         return 1;
     }
 }
 
 int check_options(){
-    if(rows <= 0){
+    if(rows <= 0)
+    {
         fprintf(stderr, "incorrect rows number\n");
         return 1;
     }
-    if(columns <= 0){
+    if(columns <= 0)
+    {
         fprintf(stderr, "incorrect colums number\n");
         return 1;
     }
-    if(min_weight < 0 || max_weight < 0){
+    if(min_weight < 0 || max_weight < 0)
+    {
         fprintf(stderr, "incorrect lower limit or incorrect higher limit\n");
         return 1;
     }
-    if(max_weight < min_weight){
+    if(max_weight < min_weight)
+    {
         fprintf(stderr, "higher limit is too low\n");
         return 1;
     }
-    for(int i=0; i<2*npaths; i++){
-        if(paths[i]<0){
+    for(int i=0; i<2*npaths; i++)
+    {
+        if(paths[i]<0)
+	{
             fprintf(stderr, "node %d does not exist\n", paths[i]);
             return 1;
         }
@@ -134,16 +165,20 @@ int check_options(){
     return 0;
 }
 
-int main  (int argc ,char** argv){
+int main  (int argc ,char** argv)
+{
 	srand(time(NULL));
-    if(get_options(argc, argv) != 0){
+    if(get_options(argc, argv) != 0)
+    {
         fprintf(stderr, "incorrect input\n");
         exit (EXIT_FAILURE);
     }
-    if(check_options() != 0){
+    if(check_options() != 0)
+    {
         exit (EXIT_FAILURE);
     }
-    if(mode == cgenerate){      //generate
+    if(mode == cgenerate)
+    {   
 
         Node *graph = generate(max_weight, min_weight, columns, rows, edges);
         write (columns, rows, graph, output);
@@ -155,7 +190,8 @@ int main  (int argc ,char** argv){
 		free(graph);
         exit (EXIT_SUCCESS);
     }
-    else if(mode == checkconnection){     //check-connection
+    else if(mode == checkconnection)
+    {    
 	
 		FILE *in = fopen(input,"r");
 		if (in == NULL) 
@@ -175,13 +211,18 @@ int main  (int argc ,char** argv){
 			free(graph[i].weight);
 		}
 		free(graph);
-        if(x == 0){
+        if(x == 0)
+	{
             printf("Graph is coherent\n");
             exit(EXIT_SUCCESS);
-        }else if(x == 1){
+        }
+	    else if(x == 1)
+	    {
             printf("Graph is incoherent\n");
             exit(EXIT_SUCCESS);
-        }else{
+        }
+	    else
+	    {
             exit(EXIT_FAILURE);
         }
     }
@@ -191,15 +232,18 @@ int main  (int argc ,char** argv){
 	
 	
 	
-    else if(mode == cpath){     //path
+    else if(mode == cpath)
+    {    
         printf("Paths to find:\n");
-        for (int i=0; i<2*npaths; i+=2){
+        for (int i=0; i<2*npaths; i+=2)
+	{
             printf("    %d -> %d\n", paths[i], paths[i+1]);
         }
         printf("Will be implemented soon :)\n");
         exit(EXIT_SUCCESS);
     }
-    else{
+    else
+    {
         fprintf(stderr, "mode not chosen\n");
         exit (EXIT_FAILURE);
     }
